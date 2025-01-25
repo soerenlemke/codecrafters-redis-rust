3e6e912531d6bb5a1f8f0a5c0595c00455cd37a5
soerenlemke <soerenlemke@gmx.de>
2025-01-25 22:39:47 +0100

    rewrite main using tokio with the example of tokio crate documentation -> A simple TCP echo server

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 };
 
                 // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                if let Err(e) = socket.write_all(b"+PONG\r\n").await {
                     eprintln!("failed to write to socket; err = {:?}", e);
                     return;
                 }
