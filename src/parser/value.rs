d08413d3986f341782a4581e0a3b236173053928
soerenlemke <soerenlemke@gmx.de>
2025-01-26 15:13:57 +0100

    Implement the ECHO command - working on the parser

--- src/parser/value.rs
+++ src/parser/value.rs
@@ -1,8 +1,8 @@
-﻿use nom::character::complete::{i64, one_of};
+﻿use nom::character::complete::{i32, i64, one_of};
 use nom::{error_position, IResult};
 use nom::error::{Error, ErrorKind};
-use nom::bytes::complete::{tag, take_while};
-use nom::combinator::{complete, eof, value};
+use nom::bytes::complete::{tag, take, take_while};
+use nom::combinator::{complete, eof, value, verify};
 use nom::sequence::terminated;
 
 #[derive(Debug, PartialEq)]
@@ -12,6 +12,7 @@ pub enum Value<'a> {
     Integer(i64),
     BulkString(&'a str),
     Array(Vec<Value<'a>>),
+    Null,
 }
 
 pub fn parse_value(input: &str) -> IResult<&str, Value> {
@@ -21,7 +22,7 @@ pub fn parse_value(input: &str) -> IResult<&str, Value> {
         '+' => parse_simple_string,
         '-' => parse_error,
         ':' => parse_integer,
-        '$' => todo!(),
+        '$' => parse_bulk_string,
         _ => unreachable!("Invalid type char"),
     };
 
@@ -37,6 +38,19 @@ fn parse_simple_string_raw(input: &str) -> IResult<&str, &str> {
     terminated(take_while(|c| c != '\r' && c != '\n'), crlf)(input)
 }
 
+fn parse_bulk_string(input: &str) -> IResult<&str, Value> {
+    let (input, length) = terminated(i32, crlf)(input)?;
+    if length == -1 {
+        return Ok((input, Value::Null))
+    }
+    if length < -1 {
+        return Err(nom::Err::Error(Error::new(input, ErrorKind::Verify)));
+    }
+
+    let (input, value) = terminated(take(length as usize), crlf)(input)?;
+    Ok((input, Value::BulkString(value)))
+}
+
 fn parse_error(input: &str) -> IResult<&str, Value> {
     let (input, value) = parse_simple_string_raw(input)?;
     Ok((input, Value::Error(value)))
@@ -47,7 +61,7 @@ fn parse_integer(input: &str) -> IResult<&str, Value> {
     Ok((input, Value::Integer(value)))
 }
 
-fn crlf(input: &str) -> IResult<&str, &str, Error<&str>> {
+fn crlf(input: &str) -> IResult<&str, &str> {
     tag("\r\n")(input)
 }
 
@@ -78,5 +92,14 @@ mod tests {
         assert_eq!(parse_value(":-1000\r\n"), Ok(("", Value::Integer(-1000))));
         assert!(parse_value(":-1000\n").is_err());
         assert!(parse_value(":-1000\r\nTHIS_SHOULD_NOT_BE_HERE").is_err());
+    }
+
+    #[test]
+    fn test_parse_bulk_string() {
+        assert_eq!(parse_value("$6\r\nfoobar\r\n"), Ok(("", Value::BulkString("foobar"))));
+        assert_eq!(parse_value("$0\r\n\r\n"), Ok(("", Value::BulkString(""))));
+        assert_eq!(parse_value("$-1\r\n"), Ok(("", Value::Null)));
+        assert!(parse_value("$-2\r\n").is_err());
+        assert!(parse_value("$10\r\n123456789\r\n").is_err());
     }
 }
